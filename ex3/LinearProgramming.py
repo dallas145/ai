11477b6b9a043e093eb236bbe6f53f1422184642
dallas145 <mikelg512@gmail.com>
2024-05-16 21:43:27 +0800

    add ex3
    ex3 done

--- ex3/LinearProgramming.py
+++ ex3/LinearProgramming.py
@@ -1,3 +1,4 @@
+import random
 # linear programming
 # Max 3x + 2y + 5z
 # s.t. x+y <= 10
@@ -11,10 +12,31 @@
 #      x, y >= 0
 
 def f(p):
-    x = p[0]
-    y = p[1]
-    z = p[2]
-    return 3*x + 2*y + 5*z
+    x, y, z = p[0], p[1], p[2]
+    if x + y <= 10 and 2*x + z <= 9 and y + 2*z <= 11 and x >= 0 and y >= 0 and z >= 0:
+        return 3*x + 2*y + 5*z
+    else:
+        return -1
 
+
+p = [0, 0, 0]
+pv = 0
+fail = 0
+
+while fail < 100000:
+    nP = p.copy()
+    for i in range(len(nP)):
+        nP[i] += random.uniform(-1, 1)
+    nPv = f(nP)
     
-print(f([1, 2, 3]))
+    if nPv > pv:
+        p = nP
+        pv = nPv
+        print(pv)
+    else:
+        fail += 1
+        
+    
+print()
+print(f'max= {pv:.4f}')
+print(f'x= {p[0]:.4f}, y= {p[1]:.4f}, z= {p[2]:.4f}')
